15b35ccb6bbf3110a0db957b514ad59bac50915f
Chris DeMars <cdemars@digitalocean.com>
2024-07-19 14:34:42 -0400

    adds prompt

--- pages/api/generateInfo.js
+++ pages/api/generateInfo.js
@@ -1,5 +1,5 @@
 // import the Configuration class and the OpenAIApi class from the openai npm module
-import { Configuration, OpenAIApi } from 'openai';
+const { Configuration, OpenAIApi } = require('openai');
 
 // create a new configuration object that includes the api key and uses the Configuration class from the openai module
 const configuration = new Configuration({
@@ -9,24 +9,23 @@ const configuration = new Configuration({
 // create a new instance of the OpenAIApi class and pass in the configuration object
 const openai = new OpenAIApi(configuration);
 
-// create a new async function called generateInfo that accepts a request and response object as parameters
-export default async function generateInfo(req, res) {
+// create an async function called generateInfo that accespts a request and response object as parameters
+const generateInfo = async (req, res) => {
   // try to run the following code block
   try {
-    // destructure the prompt and max_tokens from the request body
-    const { prompt, max_tokens } = req.body;
+    // destructure the prompt from the request body
+    const { prompt } = req.body;
 
-    // create a new response object by calling the completion.create method from the openai instance
-    const response = await openai.completion.create({
-      engine: 'davinci',
-      prompt,
-      max_tokens,
-    });
+    // create a new completion object using the openai instance and pass in the prompt
+    const completion = await openai.createCompletion(prompt);
 
-    // send a response back to the client with the data from the completion
-    res.status(200).json(response.data);
+    // send a response with the completion data
+    res.status(200).json({ data: completion.data });
   } catch (error) {
-    // if an error occurs, send a response back to the client with the error message
+    // if there is an error, catch it and send a response with the error message
     res.status(500).json({ error: error.message });
   }
-}
+};
+
+// export the generateInfo function as a module
+module.exports = generateInfo;
