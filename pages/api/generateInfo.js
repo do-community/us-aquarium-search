90899de26a45f8f0a46fc9d9c6b37fa6d84b2c71
Chris DeMars <cdemars@digitalocean.com>
2024-07-30 12:19:17 -0400

    updates generateInfo function

--- pages/api/generateInfo.js
+++ pages/api/generateInfo.js
@@ -1,5 +1,6 @@
 // import the Configuration class and the OpenAIApi class from the openai npm module
 import { Configuration, OpenAIApi } from 'openai';
+const { marineInfoPrompt } = require('../../data/prompt.json');
 
 // create a new Configuration object that includes the api key and sues the Configuration class from the openai module
 const configuration = new Configuration({
@@ -13,28 +14,40 @@ const openai = new OpenAIApi(configuration);
 // use try to make a request to the OpenAI completetion api and return the response
 // use catch to catch any errors and return a 500 status code with the error message
 const generateInfo =  async (req, res) {
+  const { marineInfo } = req.body;
   try {
-    const response = await openai.completions.create({
-      engine: 'davinci',
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: 'user', content: `${marineInfoPrompt}${marineInfo}` },
+      ],
       prompt: req.body.prompt,
-      max_tokens: 64,
-      temperatur: 0.9,
-      topP: 1,
-      presencePenalty: 0,
-      frequencyPenalty: 0,
-      bestOf: 1,
+      max_tokens: 200,
+      temperature: 0,
       n: 1,
-      stream: false,
-      stop: ['\n'],
     });
-    res.status(200).json(response);
+
+    const response = completion.data.choices[0].message.content;
+
+    res.status(200).json({
+      success: true,
+      data: response,
+    });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    console.log(error);
+    if (error.response.status === 401) {
+      return res.status(401).json({
+        error: "Please provide a valid API key",
+      });
+    }
+    return res.status(500).json({
+      error: "An error occurred while generating the marine information you were surfing for.",
+    });
   }
 }
 
 // export the generateInfo function as a module
-export default generateInfo;
+module.exports = generateInfo;
 
 
 
