ff1a5d2aef40813a5ed47fb9208a12b4a0262674
Chris DeMars <cdemars@digitalocean.com>
2024-07-30 15:23:32 -0400

    fixes response

--- pages/api/generateInfo.ts
+++ pages/api/generateInfo.ts
@@ -12,9 +12,9 @@ const openai = new OpenAIApi(configuration);
 const generateInfo = async (req: Request, res: Response) => {
   const { marineInfo } = req.body;
   try {
-    const completion = await openai.createCompletion({
+    const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      message: [
+      messages: [
         { role: 'user', content: `${marineInfoPrompt}${marineInfo}` },
       ],
       max_tokens: 200,
@@ -22,7 +22,7 @@ const generateInfo = async (req: Request, res: Response) => {
       n: 1,
     });
 
-    const response = completion.data.choices[0].message.content;
+    const response = completion?.data?.choices?.[0]?.message?.content;
 
     res.status(200).json({
       success: true,
@@ -30,7 +30,7 @@ const generateInfo = async (req: Request, res: Response) => {
     });
   } catch (error) {
     console.log(error);
-    if (error.response.status === 401) {
+    if ((error as any).response.status === 401) {
       return res.status(401).json({
         error: "Please provide a valid API key",
       });
