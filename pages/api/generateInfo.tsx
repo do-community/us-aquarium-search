8ad01d94a2822bdb8f211a13a2aa229a742bcf9c
Chris DeMars <cdemars@digitalocean.com>
2024-07-30 13:08:16 -0400

    updates nodemon for ts

--- pages/api/generateInfo.tsx
+++ pages/api/generateInfo.tsx
@@ -1,4 +1,5 @@
-import { Configuration, OpenAIApi } from 'openai';
+import Configuration from 'openai';
+import { OpenAIApi } from 'openai';
 import { marineInfoPrompt } from '../../data/prompt.json';
 import { Request, Response } from 'express';
 
